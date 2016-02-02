869ab86d3e367f8331726ed8d21d1d559bfe12e2
Matt Stjernholm <thew88@gmail.com>
2016-02-01 21:04:58 -0700

    delete idea compelete

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -1,17 +1,24 @@
-$( "#fetch-ideas" ).click(function() {
+$(document).ready(function() {
   fetchIdeas();
-});
+  createIdea();
+  deleteIdea();
+})
+
 
 function renderIdea(idea) {
   $("#latest-ideas").append(
     "<div class='idea' data-id='"
     + idea.id
-    + "'><h6>Published on "
+    + "'><h3>Title: "
+    + idea.title
+    + "</h3><h6>Body: "
+    + idea.body
+    + "</h5><h6>Quality: "
+    + idea.quality
+    + "</p><p>Posted at: "
     + idea.created_at
-    + "</h6><p>"
-    + idea.description
     + "</p>"
-    + "<button id='delete-idea' name='button-fetch' class='btn btn-default btn-xs'>Delete</button>"
+    + "<button id='delete-idea' name='button-delete' class='btn btn-danger btn-xs'>Delete</button>"
     + "</div>"
     )
 }
@@ -22,7 +29,7 @@ function fetchIdeas() {
   $.ajax({
     type:    "GET",
     crossDomain: true,
-    url:     "http://matts-idea-box-js.herokuapp.com/api/v1/ideas.json",
+    url:     "api/v1/ideas.json",
     success: function(ideas) {
       $.each(ideas, function(index, idea) {
         if (isNaN(newestItemID) || idea.id > newestItemID) {
@@ -33,5 +40,50 @@ function fetchIdeas() {
     error: function(xhr) {
       console.log(xhr.responseText)
     }
+  })
+}
+
+
+function createIdea() {
+  $("#save-idea").on("click", function() {
+    var ideaParams = {
+      idea: {
+        title: $("#idea-title").val(),
+        body: $("#idea-body").val()
+      }
+    }
+
+    $.ajax({
+      type:    "POST",
+      url:     "api/v1/ideas.json",
+      data:    ideaParams,
+      success: function(newIdea) {
+        renderIdea(newIdea)
+        $("#idea-body").val(""),
+        $("#idea-title").val("")
+      },
+      error: function(xhr) {
+        console.log(xhr.responseText)
+      }
+    })
+  })
+}
+
+
+
+function deleteIdea() {
+  $('#latest-ideas').delegate('#delete-idea', 'click', function() {
+    var $idea = $(this).closest(".idea")
+
+    $.ajax({
+      type: 'DELETE',
+      url: 'api/v1/ideas/' + $idea.attr('data-id') + ".json",
+      success: function() {
+        $idea.remove()
+      },
+      error: function(xhr) {
+        console.log(xhr.responseText)
+      }
+    })
   })
 }
--- app/controllers/api/v1/ideas_controller.rb
+++ app/controllers/api/v1/ideas_controller.rb
@@ -6,11 +6,28 @@ class Api::V1::IdeasController < ApplicationController
   end
 
   def create
+    @idea = Idea.new(idea_params)
+
+    if @idea.save
+      respond_with :api, :v1, @idea
+    else
+      # how to handle AJAX error?
+    end
   end
 
   def destroy
+    Idea.find(params[:id]).destroy
+    render :nothing => true
   end
 
   def update
   end
+
+  private
+
+  def idea_params
+    params.require(:idea).permit(:title, :body)
+  end
+
+
 end
