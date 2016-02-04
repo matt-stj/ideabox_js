973e4af2d19d25be829073bf2a028ef65e190942
Matt Stjernholm <thew88@gmail.com>
2016-02-03 18:12:49 -0700

    fix update controller test

--- test/controllers/api/v1/ideas_controller_test.rb
+++ test/controllers/api/v1/ideas_controller_test.rb
@@ -92,16 +92,15 @@ class Api::V1::IdeasControllerTest < ActionController::TestCase
   test "#update can increment quality" do
     assert_equal "swill", idea.quality
 
-    get :update, format: :json, id: idea.id, ideaParams: {"ideaQualityChange"=>{"quality"=>"1"}}
+    get :update, format: :json, id: idea.id, idea: {"quality"=>"1"}
 
     assert_equal "plausible", idea.reload.quality
   end
 
   test "#update can decrement quality" do
-    skip  
     idea.quality = 1
 
-    get :update, format: :json, id: idea.id, ideaParams: {"ideaQualityChange"=>{"quality"=>"-1"}}
+    get :update, format: :json, id: idea.id, idea: {"quality"=>"0"}
 
     assert_equal "swill", idea.reload.quality
   end
